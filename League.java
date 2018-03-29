db2325ad33b8012a4a90c9f206bf7073e2f205df
wfento5857 <wcfenton12@gmail.com>
2018-03-29 12:32:29 -0400

    er

--- League.java
+++ League.java
@@ -5,7 +5,40 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class League
+public class League extends Sports
 {
-    int kills, deaths, assists, towers, totalDamage, ganks, outplays;
+    int kills, deaths, assists, towers, totalDamage, outplays, farm;
+    public League(String n, String s, int st, int g, int p, int a, int w, int k, int d, int as, int to, int tot, int out, int cs){
+        super(n,s,st,g,p,a,w);
+        kills = k;
+        deaths = d;
+        assists = as;
+        totalDamage = tot;
+        outplays = out;
+        farm = cs;
+    }
+    public double killsPerGame(){
+        return kills/games;
+    }
+    public double avgKD(){
+        return kills/deaths;
+    }
+    public double farmPerGame(){
+        return farm/games;
+    }
+    public double damagePerGame(){
+        return totalDamage/games;
+    }
+    public double assistsPerGame(){
+        return assists/games;
+    }
+    public double outplaysPerGame(){
+        return outplays/games;
+    }
+    public double KPGame(){
+        return (kills+assists)/games;
+    }
+    public toString(){
+        
+    }
 }
--- Volleyball.java
+++ Volleyball.java
@@ -27,4 +27,5 @@ public class Volleyball extends Sports
     public double acesPerGame(){
         return aces/games;
     }   
+    
 }
